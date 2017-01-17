3b079ea9a31f3f5294b6beec061eb0108587fde5
Joshua Galbraith <galbraithjt@gmail.com>
2017-01-17 11:43:47 -0800

    Initial Commit

---
+++ Battleship/Battleship/Battleship/ComputerPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class ComputerPlayer : Player
+    {
+        
+        public Piece submarine;
+        public Piece patrol;
+        public Piece carrier;
+        public Piece cruiser;
+        public Piece battleship;
+        public Piece[] ships;
+        public int totalShips;
+
+        
+        public ComputerPlayer()
+        {
+            submarine = new Piece();
+            patrol = new Piece();
+            carrier = new Piece();
+            cruiser = new Piece();
+            battleship = new Piece();
+            ships = new Piece[5];
+            totalShips = ships.Length;
+        }
+
+        public ComputerPlayer(System.Windows.Point submarine, System.Windows.Point patrol,
+                              System.Windows.Point carrier, System.Windows.Point cruiser,
+                              System.Windows.Point battleship)
+        {
+            setShips(submarine, patrol, carrier, cruiser, battleship);
+        }
+        public void setShips(System.Windows.Point submarine, System.Windows.Point patrol,
+                              System.Windows.Point carrier, System.Windows.Point cruiser,
+                              System.Windows.Point battleship)
+        {
+            this.submarine = new Piece(submarine.X, submarine.Y, 3, "Submarine");
+            this.patrol = new Piece(patrol.X, patrol.Y, 2, "Patrol Boat");
+            this.carrier = new Piece(carrier.X, carrier.Y, 5, "Carrier");
+            this.cruiser = new Piece(cruiser.X, cruiser.Y, 3, "Cruiser");
+            this.battleship = new Piece(battleship.X, battleship.Y, 4, "Battleship");
+
+            ships = new Piece[5];
+            ships[0] = this.patrol;
+            ships[1] = this.submarine;
+            ships[2] = this.carrier;
+            ships[3] = this.cruiser;
+            ships[4] = this.battleship;
+
+            totalShips = ships.Length;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Battleship/Battleship/Battleship/Grid.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Grid
+    {
+        public Piece[] playerShips;
+        public int shipsSunk;
+        public int numHits;
+        public int numMiss;
+        public int gridWidthStart;
+        public int gridHeightStart;
+        public int gridWidthEnd;
+        public int gridHeightEnd;
+        public int removeLocation { get; set; }
+        public double x_fire_coordinate { get; set; }
+        public double y_fire_coordinate { get; set; }
+        public Grid()
+        {
+            playerShips = new Piece[5];
+           
+
+            shipsSunk = 0;
+            numHits = 0;
+            numMiss = 0;
+            gridWidthStart = 329;
+            gridHeightStart = 88;
+            gridWidthEnd = 529;
+            gridHeightEnd = 288;
+            x_fire_coordinate = 0;
+            y_fire_coordinate = 0;
+            removeLocation = 0;
+    }
+        public void algorithm()
+        {
+           // Math.Floor((495 - 329) / 20);
+            //Take the start point of the grid(left/top (the x/y) ) and 
+            //subtract the x/y point from the coorisponding side.
+            //Then divide that from the dimensions of the grid(size of the square)
+            //Finally use Math.floor to remove the decimal 
+           // Gridstart - point / dimension = value
+           // math.floor(value)
+            int width = 0;
+            int height = 0;
+
+            //Because the grid is 20 by 20 for each square, 
+            //the square selected will be switched with the
+            //1 in this example 
+            //of the grid to select the correct square
+            //Width = A-J
+            //Height = 1 - 10
+            width = (20 * 1);
+            height = (20 * 1);
+
+        }
+        public void isHit(Location coordinate)
+        {
+
+        }
+        public void reportShipHealth(Piece inputShip)
+        {
+
+        }
+        public void updateDisplay()
+        {
+
+        }
+      
+       public void storeFireLocation(double x_coordinate, double y_coordinate)
+        {
+            x_fire_coordinate = x_coordinate;
+            y_fire_coordinate = y_coordinate;
+        }
+
+    }
+}
